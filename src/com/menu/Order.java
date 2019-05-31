881f3e76c300b0d36d825214f2cc7301dffe2aca
Chakib Crypton <chakib.paris16@gmail.com>
2019-05-31 19:49:16 +0200

    Ajout de la méthode displaySelectedSide

--- src/com/menu/Order.java
+++ src/com/menu/Order.java
@@ -21,27 +21,70 @@ public class Order {
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
+    
+    
     public void displaySelectedMenu(int nbMenu) {
      int i = 0;
      while(i == 0){
-		if(nbMenu >= 1 && nbMenu <= 3) {
 			 switch(nbMenu) {
 			   case 1:
 				   String nom_1 = "poulet";
 				   System.out.println("Vous avez choisi le menu: " + nom_1 +" . Excellent choix.");
-			
+				   i++;
+			       break;
 			   case 2:
 				   String nom_2 = "boeuf";
 				   System.out.println("Vous avez choisi le menu: " + nom_2 +" . Excellent choix.");
-			
+				   i++;
+			       break;
 			   case 3:
 				   String nom_3 = "végétarien";
 				   System.out.println("Vous avez choisi le menu: " + nom_3 +" . Excellent choix.");
+				   i++;
+				   break;
+			   default:
+				   System.out.println("Vous n'avez pas choisie parmis les menues proposés.");
 			}
 			   
-               i++;
-    	    } else {
-    	    	System.out.println("Le nombre que vous avez écris ne correspond pas à un menu, veuillez réessayer");
-    	    }}
-    }
+               
+    	     }}
+    
+     public void displaySelectedSide(int nbSide, boolean allSidesEnable) {
+    	
+    	  
+    	 int i = 0; 
+    while(i == 0) {
+    if(allSidesEnable) {
+    	 switch(nbSide) {
+		   case 1:
+			   String nom_1 = "légumes_fraies";
+			   System.out.println("Vous avez choisi le menu: " + nom_1 +" . Excellent choix.");
+			   i++;
+		       break;
+		   case 2:
+			   String nom_2 = "des frites";
+			   System.out.println("Vous avez choisi le menu: " + nom_2 +" . Excellent choix.");
+			   i++;
+		       break;
+		   case 3:
+			   String nom_3 = "ou du riz";
+			   System.out.println("Vous avez choisi le menu: " + nom_3 +" . Excellent choix.");
+			   i++;
+			   break;
+		   default:
+			   System.out.println("Vous n'avez pas choisies parmi les accompagnments disponibles ");
+		}}else {
+			 switch (nbSide) {
+	            case 1:
+	                System.out.println("Vous avez choisi comme accompagnement : riz");
+	                break;
+	            case 2:
+	                System.out.println("Vous avez choisi comme accompagnement : pas de riz");
+	                break;
+	            default:
+	                System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+	                break;
+	        }
+		}
+    } }
 }
